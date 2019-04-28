37f994e1f9748393498ddbc7496df365e7f48a8c
dzh <dzh_11@qq.com>
2019-04-28 18:37:10 +0800

    opt OSSService

--- jframe-plugin/jframe-aliyun/src/main/java/jframe/aliyun/service/oss/OSSServiceImpl.java
+++ jframe-plugin/jframe-aliyun/src/main/java/jframe/aliyun/service/oss/OSSServiceImpl.java
@@ -11,8 +11,10 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.aliyun.oss.ClientConfiguration;
 import com.aliyun.oss.OSSClient;
 
+import jframe.aliyun.AliyunField;
 import jframe.aliyun.AliyunPlugin;
 import jframe.aliyun.service.OSSService;
 import jframe.core.conf.Config;
@@ -22,6 +24,22 @@ import jframe.core.plugin.annotation.Start;
 import jframe.core.plugin.annotation.Stop;
 
 /**
+ * 
+ * https://help.aliyun.com/document_detail/32010.html?spm=a2c4g.11186623.6.745.735dc06dS65fFb
+ * 
+ * // Endpoint以杭州为例，其它Region请按实际情况填写。
+ * String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
+ * // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+ * String accessKeyId = "<yourAccessKeyId>";
+ * String accessKeySecret = "<yourAccessKeySecret>";
+ * String securityToken = "<yourSecurityToken>";
+ * 
+ * // 创建OSSClient实例。
+ * OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret, securityToken);
+ * 
+ * // 关闭OSSClient。
+ * ossClient.shutdown();
+ * 
  * @author dzh
  * @date Feb 26, 2016 11:59:58 AM
  * @since 1.0
@@ -34,22 +52,29 @@ public class OSSServiceImpl implements OSSService {
     @InjectPlugin
     static AliyunPlugin plugin;
 
-    static String FILE_OSS = "file.oss";
+    static String FILE_ALIOSS = "file.alioss";
 
     static OSSConfig _config = new OSSConfig();
 
-    private Map<String, OSSClient> client = new HashMap<String, OSSClient>();
+    private Map<String, OSSClient> clients = new HashMap<String, OSSClient>();
 
     @Start
     void start() {
         LOG.info("Start OSSService");
 
         try {
-            String file = plugin.getConfig(FILE_OSS, plugin.getConfig(Config.APP_CONF) + "/oss-config.properties");
+            String file = plugin.getConfig(FILE_ALIOSS, plugin.getConfig(Config.APP_CONF) + "/alioss.properties");
             if (!new File(file).exists()) { throw new FileNotFoundException("not found " + file); }
             _config.init(file);
             for (String id : _config.getGroupIds()) {
-                // TODO
+                String endpoint = _config.getConf(id, AliyunField.K_endpoint);
+                String accessKeyId = _config.getConf(id, AliyunField.K_accessKeyId);
+                String accessKeySecret = _config.getConf(id, AliyunField.K_accessKeySecret);
+
+                ClientConfiguration conf = new ClientConfiguration();
+                // TODO config client
+                OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret, conf);
+                clients.put(id, ossClient);
             }
         } catch (Exception e) {
             LOG.error("Start OSSService Failure!" + e.getMessage(), e);
@@ -62,7 +87,7 @@ public class OSSServiceImpl implements OSSService {
     void stop() {
         LOG.info("Stop OSSService");
 
-        if (client != null) for (Map.Entry<String, OSSClient> c : client.entrySet()) {
+        if (clients != null) for (Map.Entry<String, OSSClient> c : clients.entrySet()) {
             try {
                 c.getValue().shutdown();
             } catch (Exception e) {
@@ -73,7 +98,7 @@ public class OSSServiceImpl implements OSSService {
 
     @Override
     public OSSClient getOSSClient(String id) {
-        return client.get(id);
+        return clients.get(id);
     }
 
 }
--- jframe-plugin/jframe-umeng/src/main/java/jframe/umeng/service/UmengService.java
+++ jframe-plugin/jframe-umeng/src/main/java/jframe/umeng/service/UmengService.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import jframe.core.plugin.annotation.Service;
 
 /**
+ * https://developer.umeng.com/docs/66632/detail/68343
+ * 
  * @author dzh
  * @date Mar 4, 2016 10:12:32 PM
  * @since 1.0
@@ -15,16 +17,16 @@ import jframe.core.plugin.annotation.Service;
 @Service(clazz = "jframe.umeng.service.impl.UmengServiceImpl", id = "jframe.service.umeng")
 public interface UmengService {
 
-    void sendAndBroadcast(String groupId, String token, String ticker, String title, String text,
-            Map<String, String> custom) throws Exception;
+    void sendAndBroadcast(String groupId, String token, String ticker, String title, String text, Map<String, String> custom)
+            throws Exception;
 
-    void sendIOSBroadcast(String groupId, String token, String alert, Integer badge, String sound,
-            Map<String, String> custom) throws Exception;
+    void sendIOSBroadcast(String groupId, String token, String alert, Integer badge, String sound, Map<String, String> custom)
+            throws Exception;
 
-    void sendIOSUnicast(String groupId, String token, String alert, Integer badge, String sound,
-            Map<String, String> custom) throws Exception;
+    void sendIOSUnicast(String groupId, String token, String alert, Integer badge, String sound, Map<String, String> custom)
+            throws Exception;
 
-    void sendAndUnicast(String groupId, String token, String ticker, String title, String text,
-            Map<String, String> custom) throws Exception;
+    void sendAndUnicast(String groupId, String token, String ticker, String title, String text, Map<String, String> custom)
+            throws Exception;
 
 }
