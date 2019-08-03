e14acfc70894619da74ad079a327e32125de3d6e
dzh <dzh_11@qq.com>
2019-08-03 17:18:53 +0800

    add jframe-alipay

---
+++ jframe-plugin/jframe-alipay/src/test/java/jframe/alipay/service/TestAlipayService.java
@@ -0,0 +1,10 @@
+package jframe.alipay.service;
+
+/**
+ * @author dzh
+ * @date 2019-07-22 14:59
+ */
+public class TestAlipayService {
+
+
+}
--- jframe-plugin/jframe-aliyun/src/main/java/jframe/aliyun/service/oss/OSSServiceImpl.java
+++ jframe-plugin/jframe-aliyun/src/main/java/jframe/aliyun/service/oss/OSSServiceImpl.java
@@ -1,19 +1,10 @@
 /**
- * 
+ *
  */
 package jframe.aliyun.service.oss;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.aliyun.oss.ClientConfiguration;
 import com.aliyun.oss.OSSClient;
-
 import jframe.aliyun.AliyunField;
 import jframe.aliyun.AliyunPlugin;
 import jframe.aliyun.service.OSSService;
@@ -22,24 +13,31 @@ import jframe.core.plugin.annotation.InjectPlugin;
 import jframe.core.plugin.annotation.Injector;
 import jframe.core.plugin.annotation.Start;
 import jframe.core.plugin.annotation.Stop;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 
+ *
  * https://help.aliyun.com/document_detail/32010.html?spm=a2c4g.11186623.6.745.735dc06dS65fFb
- * 
+ *
  * // Endpoint以杭州为例，其它Region请按实际情况填写。
  * String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
  * // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
  * String accessKeyId = "<yourAccessKeyId>";
  * String accessKeySecret = "<yourAccessKeySecret>";
  * String securityToken = "<yourSecurityToken>";
- * 
+ *
  * // 创建OSSClient实例。
  * OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret, securityToken);
- * 
+ *
  * // 关闭OSSClient。
  * ossClient.shutdown();
- * 
+ *
  * @author dzh
  * @date Feb 26, 2016 11:59:58 AM
  * @since 1.0
@@ -64,7 +62,9 @@ public class OSSServiceImpl implements OSSService {
 
         try {
             String file = plugin.getConfig(FILE_ALIOSS, plugin.getConfig(Config.APP_CONF) + "/alioss.properties");
-            if (!new File(file).exists()) { throw new FileNotFoundException("not found " + file); }
+            if (!new File(file).exists()) {
+                throw new FileNotFoundException("not found " + file);
+            }
             _config.init(file);
             for (String id : _config.getGroupIds()) {
                 String endpoint = _config.getConf(id, AliyunField.K_endpoint);
@@ -76,11 +76,10 @@ public class OSSServiceImpl implements OSSService {
                 OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret, conf);
                 clients.put(id, ossClient);
             }
+            LOG.info("Start OSSService Successfully!");
         } catch (Exception e) {
-            LOG.error("Start OSSService Failure!" + e.getMessage(), e);
-            return;
+            LOG.error("Start OSSService Failed!" + e.getMessage(), e);
         }
-        LOG.info("Start OSSService Successfully!");
     }
 
     @Stop
